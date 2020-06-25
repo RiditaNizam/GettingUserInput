47301a753ef471f15a642b58ab18c0d4f1509502
Ridita Nizam <ridita.nizam@fdmgroup.com>
2020-06-24 20:00:08 -0700

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		// TEXT
+		System.out.println("Enter a name and press Enter");
+
+		// Wait for the user to enter a line of text
+		String line = input.nextLine();
+
+		System.out.println("You entered: " + line);
+
+		// WHOLE NUMBER
+		System.out.println("Enter a whole number and press Enter");
+
+		int wholeNumber = input.nextInt();
+
+		System.out.println("You entered: " + wholeNumber);
+
+		// DECIMAL
+		System.out.println("Enter a double value and press Enter");
+
+		double doubleNumber = input.nextDouble();
+
+		System.out.println("You entered: " + doubleNumber);
+
+	}
+
+}
